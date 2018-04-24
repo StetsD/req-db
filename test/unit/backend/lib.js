d6be9a49c8970f41ac30d1a205b6c714f1b3e84d
StetsD <chortstd@gmail.com>
2018-04-24 09:05:44 +0300

    lib test end

--- test/unit/backend/lib.js
+++ test/unit/backend/lib.js
@@ -1,7 +1,9 @@
 let {assert} = require('chai');
-let {createHashPass, checkPass} = require('../../../lib/credentialsHash');
+let fs = require('fs');
+let rimraf = require('rimraf');
 
 describe('Hashing password tests', () => {
+	let {createHashPass, checkPass} = require('../../../lib/credentialsHash');
 	var hashPass;
 
 	it('Should return object', async () => {
@@ -31,8 +33,33 @@ describe('Redis client tests', () => {
 	});
 });
 
-describe('Sockets serverside tests', () => {
-	let sio = require('../../../lib/socket-io');
+describe('Tools test', () => {
+	let dateTools = require('../../../lib/tools/formatDate');
+	let mkdir = require('../../../lib/tools/mkdir-r');
 
-	console.log(sio);
+
+	it('formatZero works fine', () => {
+		assert.equal(dateTools.formatZero(1), '01', 'Method must return 2 signs');
+		assert.equal(dateTools.formatZero(20), '20', 'Method must return 1 sign');
+	});
+
+	it('getDate works fine', () => {
+		let date = dateTools.getDate();
+		assert.hasAllKeys(date, ['date', 'time']);
+		assert.lengthOf(date.date, 8);
+		assert.lengthOf(date.time, 5);
+	});
+
+	it('mkdir custom method works fine', async () => {
+		let path = `megatest/supertest/test`;
+		await mkdir(path);
+		let stat = await new Promise((res, rej)=>{
+			fs.stat(path, (err, st) => {
+				if(err) throw new Error(err);
+				res(st);
+			});
+		});
+		assert.equal(stat.isDirectory(), true, 'Can`t find directory ' + path);
+		rimraf('megatest', (err) => {if(err) throw new Error(err)})
+	});
 });
